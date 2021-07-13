ee2d108b838922c0ddb11414b24c9765970d4693
kkatanono <kkatanono@outlook.jp>
2021-07-14 06:38:26 +0900

    2021/07/14

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,17 +1,50 @@
 <template>
- <div>
-   <div v-for="b in blogs" :key="b.slug">
-     <nuxt-link :to="'/articles/'+ b.slug">{{b.title}} {{b.description}}</nuxt-link>
-   </div>
- </div>
+  <div>
+    <v-container>
+      <v-row>
+        <div v-for="b in blogs" :key="b.slug">
+          <v-col>
+            <v-card class="mx-auto" max-width="344" outlined>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <!-- <div class="text-overline mb-4">OVERLINE</div> -->
+                  <v-list-item-title class="text-h5 mb-1">
+                    {{ b.title }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle>{{
+                    b.description
+                  }}</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn outlined rounded text :to="'/articles/' + b.slug">
+                  Button
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </div>
+      </v-row>
+    </v-container>
+
+    <!-- <nuxt-link :to="'/articles/' + b.slug">
+        {{ b.title }} {{ b.description }}
+      </nuxt-link> -->
+  </div>
 </template>
 <script>
 export default {
- async asyncData ({ $content, params }) {
-   const query = await $content('articles' || 'index').limit(10)
-   const blogs = await query.fetch()
-   return { blogs }
- }
+  async asyncData({ $content, params }) {
+    const query = await $content('articles' || 'index').limit(10)
+    const blogs = await query.fetch()
+    return { blogs }
+  },
 }
 </script>
-
